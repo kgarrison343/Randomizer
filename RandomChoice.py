45287e54f7becc49dc8b5dc79b6e4c4f5ae73217
Kyle Garrison <kgarrison343@gmail.com>
2014-12-22 18:41:09 -0500

    Add while loop to allow multiple randomizations

--- RandomChoice.py
+++ RandomChoice.py
@@ -1,5 +1,4 @@
 from os import listdir
-from os.path import isfile, join
 from random import randrange
 
 
@@ -7,20 +6,27 @@ def choose_randomly(choice_list):
     length = len(choice_list)
     return choice_list[randrange(0,length)]
 
-if __name__ == "__main__":
+
+def main():
     files = listdir("./Choices")
     print("What file would you like to choose from?")
     for i, file in enumerate(files):
         print(str(i + 1) + ". " + file)
 
     choice = int(input())
-    choiceFile = ''
+    choice_file = ''
     if 0 < choice < len(files) + 1:
-        choiceFile = "./Choices/" + files[choice - 1]
+        choice_file = "./Choices/" + files[choice - 1]
     else:
         print("That is not a valid number, goodbye")
 
-    with open(choiceFile) as f:
+    with open(choice_file) as f:
         choices = [x.strip('\n') for x in f.readlines()]
     print(choose_randomly(choices))
-    input("press any key to exit...")
+    choice = input("q to exit, any other to choose again\n")
+    return choice == 'q' or choice == 'Q'
+
+if __name__ == "__main__":
+    done = False
+    while not done:
+        done = main()
