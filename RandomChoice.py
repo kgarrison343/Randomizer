4e7c2ea4e64838db9b27229e99aa38d81aa3c33f
Kyle Garrison <kgarrison343@gmail.com>
2015-01-03 23:49:27 -0500

    fix type annotation

--- RandomChoice.py
+++ RandomChoice.py
@@ -1,6 +1,6 @@
 from random import randrange
 
 
-def choose_randomly(choice_list: str):
+def choose_randomly(choice_list: list):
     length = len(choice_list)
     return choice_list[randrange(0,length)]
